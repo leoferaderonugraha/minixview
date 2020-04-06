b8c254c5b24baa518843e2e970c7de28b6068bd1
leoferaderonugraha <leoferaderonugraha@gmail.com>
2020-04-06 18:31:38 +0700

    now works with stdin

--- functions.h
+++ functions.h
@@ -7,4 +7,9 @@ int get_file_size2(FILE *fp);
 // cpnb = copy n byte(s)
 void cpnb(void *dst, void *src, int n);
 
+enum stats {
+  READ_FROM_STDIN,
+  READ_FROM_FILE
+};
+
 #endif
--- minixview.c
+++ minixview.c
@@ -1,54 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <ctype.h>  // for isprint()
+#include <unistd.h> // for getopt()
 
 #include "functions.h"
 
+#define MAX_BUFF 256
 
 int main(int argc, char**argv) {
+  int flag;
+  char *file_name;
+  unsigned char *content;
+  unsigned char buffer[MAX_BUFF];
+
+  int offset, line_offset;
+  int content_len = 0;
+  int bytes_copied;
+
+
   if (argc < 2){
-    printf("%s: file_name\n", argv[0]);
-    return 1;
+    flag = READ_FROM_STDIN;
+    content = malloc(sizeof(char) * MAX_BUFF);
+
+    int n = 1;
+    offset = 0;
+    while (bytes_copied = read(STDIN_FILENO, buffer, MAX_BUFF)) {
+      // if the current buffer it's not enough
+      // then reallocate some more
+      if (bytes_copied == MAX_BUFF) {
+        n++;
+        content = realloc(content, n * sizeof(char) * MAX_BUFF);
+      }
+
+      cpnb(&content[offset], buffer, MAX_BUFF);
+      offset += bytes_copied;
+      content_len += bytes_copied;
+    }
   }
+  else {
+    int opt;
+    flag = READ_FROM_FILE;
 
-  char *file_name = argv[1];
-  FILE *fp = fopen(file_name, "rb");
-  int file_size = get_file_size(file_name);
+    while ((opt = getopt(argc, argv, "f:")) != -1) {
+      switch (opt) {
+        case 'f':
+          file_name = optarg;
+          printf("file: %s\n", optarg);
+          break;
+        case '?':
+          break;
+      }
+    }
 
+    FILE *fp = fopen(file_name, "rb");
+    int file_size = get_file_size(file_name);
 
-  unsigned char *content = malloc(sizeof(char) * file_size);
-  unsigned char buffer[256];
+    content = malloc(sizeof(char) * file_size);
 
-  if (content == NULL) {
-    printf("Fail to allocating the required memory.");
-    return 1;
-  }
+    if (content == NULL) {
+      fprintf(stderr, "Fail to allocating the required memory.\n");
+      return 1;
+    }
 
-  int copied = 0;
-  int offset = 0;
-  int bytes_to_copy = file_size;
+    bytes_copied = 0;
+    offset = 0;
+    int bytes_to_copy = file_size;
 
-  // read the whole file content and store it to *content
-  while (bytes_to_copy > 0) {
-    copied = fread(&buffer, 1, 256, fp);
-    bytes_to_copy -= copied;
+    // read the whole file content and store it to *content
+    while (bytes_to_copy > 0) {
+      bytes_copied = fread(&buffer, 1, 256, fp);
+      bytes_to_copy -= bytes_copied;
 
-    cpnb(&content[offset], buffer, copied);
-    offset += copied;
+      cpnb(&content[offset], buffer, bytes_copied);
+      offset += bytes_copied;
+    }
+    fclose(fp);
   }
 
   // set offset back to zero
   offset = 0;
-  int line_offset = 0;
+  line_offset = 0;
+  if (flag == READ_FROM_FILE) {
+    content_len = get_file_size(file_name);
+  }
 
   // display the content in a hex value
-  for(int stat = 0, total_ch = 0, total_space = 0; (file_size - offset) > 0;) {
+  for(int stat = 0, total_ch = 0, total_space = 0; (content_len - offset) > 0;) {
     // printing its offset
     printf("%08x:    ", line_offset);
 
     for (int i = 0; i < 8 && !stat; i++) {
       for (int j = 0; j < 2 && !stat; j++) {
-        if ((offset + j + 1) >= file_size) stat = 1;
+        if ((offset + j + 1) >= content_len) stat = 1;
 
         total_ch++;
         printf("%02x", content[offset + j]);
@@ -83,5 +126,4 @@ int main(int argc, char**argv) {
   }
 
   free(content);
-  fclose(fp);
 }
