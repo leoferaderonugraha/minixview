fb6e30df82f7ba72db7af645ca41f91c781088e7
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2020-04-06 19:17:51 +0700

    Optimizing the test cases

--- test_case.rb
+++ test_case.rb
@@ -1,9 +1,12 @@
+path = './test_cases/'
+
+if (!Dir.exist?(path)) then
+  Dir.mkdir(path)
+end
+
 (1..16).each do |i|
   File.open("./test_cases/test_case#{i}", 'w') do |fp|
     fp.write(((64+i).chr)*i)
   end
-end
-
-(1..16).each do |i|
-  print `./mxview ./test_cases/test_case#{i}`
+  print `./mxview -f ./test_cases/test_case#{i}`
 end
