e7efa0d2ebbf9d4f1534af94f81e301ca6bcadd6
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2020-05-18 00:29:22 +0700

    updating the software version

--- swinfo.h
+++ swinfo.h
@@ -1,9 +1,12 @@
-#ifndef SWINFO
-#define SWINFO
+/* This file is created for software versioning only */
 
-#ifndef SW_VERSION
-#define SW_VERSION "0.9.3"
-#endif
+/* prevent included twice */
+#ifndef SWINFO_H_
+#define SWINFO_H_
+
+// #ifndef SW_VERSION
+// #define SW_VERSION "0.9.3"
+// #endif
 
 #ifndef SW_AUTHOR
 #define SW_AUTHOR "Leo Feradero Nugraha"
@@ -13,4 +16,18 @@
 #define SW_DATE "06Apr2020"
 #endif
 
+// SW_VERSION[MAX_MAJOR][MAX_MINOR][STR_LEN]
+static const char *SW_VERSION[][10] = {
+  /*from the latest to the oldest */
+  {"0.10.0"},
+  {"0.9.4", "0.9.3"}
+};
+
+// const char SW_VERSION[][10] = {
+//   "0.10.0",
+//   "0.9.4",
+//   "0.9.3"
+// };
+
+/* SWINFO_H_ */
 #endif
